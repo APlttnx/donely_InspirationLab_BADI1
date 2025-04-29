3975f5ab97f955435428d0b8df7409e95fd8db29
Arnout Plettinx <arnoutplettinx@hotmail.com>
2025-04-29 10:35:28 +0200

    Add project files.

---
+++ Classes/User.cs
@@ -0,0 +1,28 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.ComponentModel.DataAnnotations;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace donely_Inspilab.Classes
+//{
+//    public class User
+//    {
+//        public string Username { get; set; }
+        
+//        public string Email { get; set; }
+//        public string TelephoneNumber { get; set; }
+//        public string HashPassword { get; set; }
+//        public int Id { get; set; }
+
+//        public User(string _userName, string _email, string _telephoneNumber, string _hashPassword, int _id)
+//        {
+//            UserName = _userName;
+//            Email = _email;
+//            TelephoneNumber = _telephoneNumber;
+//            Password = _password;
+//            Id = _id;
+//        }
+//    }
+//}
---
+++ Pages/RegisterPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using donely_Inspilab.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace donely_Inspilab.Pages
+{
+    /// <summary>
+    /// Interaction logic for RegisterPage.xaml
+    /// </summary>
+    public partial class RegisterPage : Page
+    {
+        public RegisterPage()
+        {
+            InitializeComponent();
+        }
+        private void btnRegister_Click(object sender, RoutedEventArgs e)
+        {
+            //check een van velden leeg
+            if (txtName.Text.Trim() == "" || txtEmail.Text.Trim() == "" || txtTelnr.Text.Trim() == "" || txtPassword.Text.Trim() == "" || txtConfirmPassword.Text.Trim() == "")
+            {
+                MessageBox.Show("Not all required fields are filled in", "Incomplete Registration", MessageBoxButton.OK);
+                return;
+            }
+
+            UserRegistration newUser = new(txtName.Text, txtEmail.Text, txtTelnr.Text, txtPassword.Text, txtConfirmPassword.Text);
+            newUser.ValidateFields();
+            MessageBox.Show(newUser.Email);
+
+
+
+            
+        }
+    }
+}
