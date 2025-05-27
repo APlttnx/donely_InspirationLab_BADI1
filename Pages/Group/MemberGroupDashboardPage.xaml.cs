3d8aeb70b52f9be6ffa00e039929757f0dc33c7f
Arnout Plettinx <arnoutplettinx@hotmail.com>
2025-05-27 11:28:04 +0200

    MemberDashboard visuals + load items

--- Pages/Group/MemberGroupDashboardPage.xaml.cs
+++ Pages/Group/MemberGroupDashboardPage.xaml.cs
@@ -21,16 +21,49 @@ namespace donely_Inspilab.Pages.Group
     /// </summary>
     public partial class MemberGroupDashboardPage : Page
     {
+
+        private GroupMember CurrentMember;
+        private List<GroupMember> OtherMembers;
         public MemberGroupDashboardPage()
         {
             InitializeComponent();
-            lblGroupName.Content = GroupState.LoadedGroup.Name;
-            imgGroupImage.Source = GroupState.LoadedGroup.ImageSource;
+            LoadItems();
+            
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+
+        private void LoadItems()
+        {
+
+
+            //groupmember splitsen in current member en rest
+            CurrentMember = (GroupMember)GroupState.LoadedGroup.Members.FirstOrDefault(t => t.UserId == SessionManager.GetCurrentUserID());
+            OtherMembers = GroupState.LoadedGroup.Members.Where(t => t.UserId != SessionManager.GetCurrentUserID()).ToList(); //Huidige user uit de memberlijst halen
+
+            //taken laden
+            CurrentMember = TaskService.LoadTaskInstances(CurrentMember);
+
+            //Listviews en labels invullen
+            lblGroupName.Content = GroupState.LoadedGroup.Name;
+            imgGroupImage.Source = GroupState.LoadedGroup.ImageSource;
+            lsvMembersOverview.ItemsSource = OtherMembers;
+            lsvMemberTasks.ItemsSource = CurrentMember.ActiveTaskList;
+            lblCurrency.Content = CurrentMember.Currency;
+        }
+        private void GoBack_Click(object sender, RoutedEventArgs e)
         {
             NavService.ToHomePage();
         }
+
+        private void SucceedTask_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void FailTask_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
     }
 }
